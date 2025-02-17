5a47bd7d9e9bb775cc30789c3c1c3913051cd540
0xArcheon <imamlan03@gmail.com>
2025-02-17 15:11:29 +0530

    experience section responsiveness adjustments

--- src/app/components/ExpCard.js
+++ src/app/components/ExpCard.js
@@ -10,61 +10,91 @@ function ExpCard() {
         className="expcard p-5 rounded-xl flex flex-col h-full gap-5
         justify-around bg-slate-400 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 my-3"
       >
-        <div className="flex gap-5 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out">
-          <div className="position text-sm font-semibold flex items-center gap-2">
-            <div>
-              <img src="./dev.png" alt="" className="h-8 invert" />
-            </div>
-            <div>
-              <div>Software Developer</div>
-              <div className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800">
-                Aug 2024-Present
+        <div
+          className="flex gap-4 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out
+        max-md:flex-col max-md:items-start max-md:gap-1"
+        >
+          <div className="">
+            <div className="position text-sm font-semibold flex items-center gap-2">
+              <div className="max-md:hidden">
+                <img src="./dev.png" alt="" className="h-8 invert" />
+              </div>
+              <div className="max-md:flex">
+                <div>
+                  <div>Software Developer</div>
+                </div>
+                <div
+                  className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800
+              max-md:hidden"
+                >
+                  Aug 24-Present
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <div className="company text-sm text-right">
+          <div className="">
+            <div className="company text-sm text-right max-md:text-left">
               National Informatics Centre
             </div>
-            <div className="text-xs text-right">Guwahati, Assam</div>
+            <div className="text-xs text-right max-md:text-left">
+              Guwahati, Assam
+            </div>
           </div>
         </div>
-        <div className="flex gap-5 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out">
+        <div className="h-0.5 bg-white hidden max-md:block opacity-20"></div>
+        <div
+          className="flex gap-4 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out
+        max-md:flex-col max-md:items-start max-md:gap-1"
+        >
           <div className="position font-semibold flex items-center gap-2 text-sm">
-            <div>
+            <div className="max-md:hidden">
               <img src="./team-leader.png" alt="" className="h-8 invert" />
             </div>
-            <div>
+            <div className="max-md:flex">
               <div>Lead Support Engineer</div>
-              <div className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800">
-                Dec 2023-Jul 2024
+              <div
+                className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800
+              max-md:hidden"
+              >
+                Dec 23-Jul 24
               </div>
             </div>
           </div>
           <div>
-            <div className="company text-sm text-right">
+            <div className="company text-sm text-right max-md:text-left">
               National Informatics Centre
             </div>
-            <div className="text-xs text-right">Guwahati, Assam</div>
+            <div className="text-xs text-right max-md:text-left">
+              Guwahati, Assam
+            </div>
           </div>
         </div>
-        <div className="flex gap-5 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out">
+        <div className="h-0.5 bg-white hidden max-md:block opacity-20"></div>
+        <div
+          className="flex gap-4 items-center justify-between transform hover:scale-110 hover:-translate-y-2 transition duration-300 ease-in-out
+        max-md:flex-col max-md:items-start max-md:gap-1"
+        >
           <div className="position font-semibold flex items-center gap-2 text-sm">
-            <div>
+            <div className="max-md:hidden">
               <img src="./support.png" alt="" className="h-8 invert" />
             </div>
-            <div>
-              <div>Application Support Engineer</div>
-              <div className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800">
-                Aug 2023-Nov 2023
+            <div className="max-md:flex">
+              <div>Support Engineer</div>
+              <div
+                className="duration text-xs opacity-60 rounded-2xl border-2 w-fit px-2 bg-slate-100 text-slate-800
+              max-md:hidden"
+              >
+                Aug 23-Nov 23
               </div>
             </div>
           </div>
           <div>
-            <div className="company text-sm text-right">
+            <div className="company text-sm text-right max-md:text-left">
               National Informatics Centre
             </div>
-            <div className="text-xs text-right">Guwahati, Assam</div>
+            <div className="text-xs text-right max-md:text-left">
+              Guwahati, Assam
+            </div>
           </div>
         </div>
       </div>
