7d8bcf9e79890746c80181330f72515e7e59589c
ImAmlan03 <imamlan03@gmail.com>
2025-12-28 16:25:57 +0530

    "lorem"

--- src/app/components/gallery/CnGallery.js
+++ src/app/components/gallery/CnGallery.js
@@ -4,43 +4,40 @@ import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-const CnGallery = ({ images }) => {
-  const items = [
-    {
-      id: 1,
-      elements: [
-        {
-          id: 1,
-          width: 250,
-          img: images[0].url,
-          alt: images[0].alt,
-        },
-        {
-          id: 2,
-          width: 100,
-          img: images[1].url,
-          alt: images[1].alt,
-        },
-      ],
-    },
-    {
-      id: 2,
-      elements: [
-        {
-          id: 3,
-          width: 100,
-          img: images[2].url,
-          alt: images[2].alt,
-        },
-        {
-          id: 4,
-          width: 250,
-          img: images[3].url,
-          alt: images[3].alt,
-        },
-      ],
-    },
-  ];
+const items = [
+  {
+    id: 1,
+    elements: [
+      {
+        id: 1,
+        width: 250,
+        img: "https://images.pexels.com/photos/29273395/pexels-photo-29273395/free-photo-of-rainy-window-view-in-belfast.jpeg",
+      },
+      {
+        id: 2,
+        width: 100,
+        img: "https://images.pexels.com/photos/27303623/pexels-photo-27303623/free-photo-of-a-kitchen-with-a-window-and-a-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+      },
+    ],
+  },
+  {
+    id: 2,
+    elements: [
+      {
+        id: 3,
+        width: 100,
+        img: "https://images.pexels.com/photos/29273395/pexels-photo-29273395/free-photo-of-rainy-window-view-in-belfast.jpeg",
+      },
+      {
+        id: 4,
+        width: 250,
+        img: "https://images.pexels.com/photos/29273395/pexels-photo-29273395/free-photo-of-rainy-window-view-in-belfast.jpeg",
+      },
+    ],
+  },
+];
+
+const CnGallery = () => {
   const [activeItem, setActiveItem] = useState(null);
 
   const allElements = items.flatMap((column) => column.elements);
@@ -50,11 +47,11 @@ const CnGallery = ({ images }) => {
   };
 
   return (
-    <div className="flex flex-col w-full justify-center">
+    <div className="flex flex-col mr-5 w-full">
       <div>
-        <div className="title text-xl font-medium w-full my-4">Screenshots</div>
+        <div className="title text-xl font-medium w-full">Screenshots</div>
       </div>
-      <div className="h-full center w-full flex flex-col gap-5 relative items-center py-10 my-3">
+      <div className="h-full center w-full flex flex-col gap-5 relative items-center py-10 my-10">
         <motion.div
           className={cn("flex flex-col gap-5")}
           layout
@@ -90,23 +87,23 @@ const CnGallery = ({ images }) => {
             <AnimatePresence mode="popLayout">
               <motion.div
                 key={activeItem.id}
-                className="w-full h-full flex items-center justify-center gap-10 overflow-hidden max-md:flex-col"
+                className="w-full h-full flex items-center justify-center gap-10 overflow-hidden"
                 transition={{ duration: 0.5, ease: "easeInOut" }}
                 layout
               >
                 <motion.div
                   layoutId={`card-${activeItem.id}`}
-                  className="w-[600px] h-[400px] rounded-3xl center font-bold text-5xl cursor-pointer overflow-hidden z-10 max-md:w-[400px] max-md:h-[150px] "
+                  className="w-[600px] h-[400px] rounded-3xl center font-bold text-5xl cursor-pointer overflow-hidden z-10"
                   onClick={() => setActiveItem(null)}
                 >
                   <img
                     src={activeItem.img}
                     alt=""
-                    className="w-full object-cover h-full max-md:object-contain"
+                    className="w-full object-cover h-full"
                   />
                 </motion.div>
                 <motion.div
-                  className="flex flex-col gap-4 justify-center items-center max-md:flex-row"
+                  className="flex flex-col gap-4 justify-center items-center"
                   animate={{ opacity: 1, x: 0 }}
                   transition={{ delay: 0.3 }}
                 >
