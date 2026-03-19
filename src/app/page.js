a7984028b2cb288f5fa3df3cbb3afa8dd4d7f989
ImAmlan03 <imamlan03@gmail.com>
2026-03-19 22:44:10 +0530

    minor ui enhancement

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import RightCard from "./components/RightCard.js";
 
 export default function Home() {
   return (
-    <div className="flex px-24 max-w-screen-xl mx-auto max-lg:flex-col max-md:px-18 max-sm:px-8">
+    <div className="flex px-48 max-w-screen-2xl mx-auto max-lg:flex-col max-md:px-18 max-sm:px-8">
       <MainCard />
       <RightCard />
     </div>
